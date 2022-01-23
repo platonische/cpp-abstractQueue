a5bf3d3bd4bd7dbcef7cf6a5c815bff4380d336a
platonische <i.platonov@belvg.com>
2022-01-23 21:33:05 +0300

    Realise 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+//#include "src/exted/Int/QueueIntValue.h"
+#include "src/exted/Any/QueueAnyValue.h"
 
+using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
+    int *array = new int[6] { 1, 2, 3, 4, 5, '\0' };
+
+    QueueAnyValue<int>* queue = new QueueAnyValue<int>();
+
+    //queue->sampleData(array);
+    queue->addElement(queue->createElement(12),queue->getLastElement());
+    queue->addElement(queue->createElement(56),queue->getLastElement());
+    queue->addElement(queue->createElement(85),NULL);
+    queue->addElement(queue->createElement(40),queue->getHead());
+    cout << queue->toString() << endl;
+    queue->changeElements(queue->findElement(56),queue->getHead());
+    cout << queue->toString() << endl;
 
 }
+
+//
+
+//QueueIntValue* queue = new QueueIntValue(array);
+
+//AbstractQueue<Element>* queue = new  AbstractQueue(array);
+//    printQueue(queue);
+
+//    queue->addElement(queue->createElement(12),queue->getLastElement());
+//    printQueue(queue);
+
+//    queue->addElement(queue->createElement(150),queue->findElement(4));
+//    printQueue(queue);
+
+//    queue->changeElements(queue->findElement(150),queue->findElement(4));
+//    printQueue(queue);
+
+//    queue->deleteElement(queue->findElement(150));
+//    printQueue(queue);
+
+//    queue->deleteAll();
+//    printQueue(queue);
+
+//    queue->addElement(queue->createElement(12),queue->getLastElement());
+//    printQueue(queue);
+
